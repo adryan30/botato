0eb8d42add8b17f02a368a68692f5c07f242c812
Adryan Almeida <adryan1990@gmail.com>
2021-07-08 00:20:52 -0300

    fix: admin clear command improvements

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,14 +1,28 @@
 import { CommandMessage, Command, Description } from "@typeit/discord";
-import { Collection, Message, MessageResolvable } from "discord.js";
+import {
+  Collection,
+  Message,
+  MessageEmbed,
+  MessageResolvable,
+} from "discord.js";
 
 export abstract class AdminService {
   @Command("clear")
   @Description("Limpa as mesagens presentes no canal")
   async clear(message: CommandMessage) {
+    let messageQuantity = 0;
     let fetched: Collection<string, Message>;
     do {
       fetched = await message.channel.messages.fetch();
-      message.channel.messages.channel.bulkDelete(fetched);
+      fetched = fetched.filter((message) => !message.pinned);
+      messageQuantity += fetched.size;
+      await message.channel.messages.channel.bulkDelete(fetched);
     } while (fetched.size > 2);
+    const embedMessage = await message.channel.send({
+      embed: new MessageEmbed()
+        .setTitle("Limpeza concluída")
+        .setDescription(`${messageQuantity} mensagens apagadas!`),
+    });
+    setTimeout(() => embedMessage.delete(), 5000);
   }
 }
