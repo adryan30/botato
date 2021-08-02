50595b2ef2af8afd0bd815ea024bce35858d7469
Adryan Almeida <adryan1990@gmail.com>
2021-08-02 13:36:11 -0300

    fix: clear command error handling

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { CommandMessage, Command, Infos, Client, Guard } from "@typeit/discord";
 import { Collection, Message, MessageEmbed } from "discord.js";
+import { differenceInCalendarDays } from "date-fns";
 import { theme } from "../config";
 import { AdminGuard } from "../guards";
 
@@ -17,13 +18,13 @@ export abstract class AdminService {
     let fetched: Collection<string, Message>;
     do {
       fetched = await message.channel.messages.fetch({ limit: 100 });
-      fetched = fetched.filter((message) => !message.pinned);
+      fetched = fetched.filter(
+        (message) =>
+          !message.pinned &&
+          differenceInCalendarDays(message.createdAt, new Date()) < 13
+      );
       messageQuantity += fetched.size;
-      await message.channel.messages.channel.bulkDelete(fetched).catch((_) => {
-        return message.reply(
-          "Não foi possível deletar todas as mensagens, esse chat tem mensagens muito antigas :("
-        );
-      });
+      await message.channel.messages.channel.bulkDelete(fetched);
     } while (fetched.size > 2);
     const embedMessage = await message.channel.send({
       embed: new MessageEmbed()
